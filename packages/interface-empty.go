3deaa756554b1a156279a08bcc56a909e5969ffe
ductran <ductran1905@gmail.com>
2018-06-16 21:58:58 +0700

    interface

---
+++ packages/interface-empty.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+
+func main() {
+	var i interface{}
+	describe(i)
+
+	i = 42
+	describe(i)
+
+	i = "hi"
+	describe(i)
+}
+
+func describe(i interface{}) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
