a9d19285a2bf7dfaf967d9a22272cb17bb12127d
ductran <ductran1905@gmail.com>
2018-06-17 11:30:46 +0700

    go routines

---
+++ packages/excercise-equivalent-binarytree.go
@@ -0,0 +1,40 @@
+package main
+import "fmt"
+import "golang.org/x/tour/tree"
+
+func Walk(t *tree.Tree, ch chan int) {
+	if t.Left != nil {
+		Walk(t.Left, ch)
+	}
+	ch <- t.Value
+	if t.Right != nil {
+		Walk(t.Right, ch)
+	}
+}
+
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+
+	for i := 0; i < 10; i++ {
+		v1 := <-ch1
+		v2 := <-ch2
+		if v1 != v2 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	// ch := make(chan int)
+	// go Walk(tree.New(1), ch)
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(<-ch)
+	// }
+
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
+}
---
+++ packages/excercise-web-crawler.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var urlMap = make(map[string]bool)
+var mux sync.Mutex
+
+func setUrlMap(key string) {
+	mux.Lock()
+	defer mux.Unlock()
+	urlMap[key] = true
+}
+
+func getUrlMap(key string) bool {
+	mux.Lock()
+	defer mux.Unlock()
+	return urlMap[key]
+}
+
+type Fetcher interface {
+	// Fetch returns the body of URL and
+	// a slice of URLs found on that page.
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+// Crawl uses fetcher to recursively crawl
+// pages starting with url, to a maximum of depth.
+func Crawl(url string, depth int, fetcher Fetcher) {
+	if depth <= 0 {
+		return
+	}
+
+	if getUrlMap(url) {
+		return
+	}
+
+	body, urls, err := fetcher.Fetch(url)
+	setUrlMap(url)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("found: %s %q\n", url, body)
+	for _, u := range urls {
+		Crawl(u, depth-1, fetcher)
+	}
+	return
+}
+
+func main() {
+	Crawl("https://golang.org/", 4, fetcher)
+}
+
+// fakeFetcher is Fetcher that returns canned results.
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+// fetcher is a populated fakeFetcher.
+var fetcher = fakeFetcher{
+	"https://golang.org/": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"https://golang.org/pkg/",
+			"https://golang.org/cmd/",
+		},
+	},
+	"https://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/cmd/",
+			"https://golang.org/pkg/fmt/",
+			"https://golang.org/pkg/os/",
+		},
+	},
+	"https://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+	"https://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+}
