4e9519acb5102310ac2d55943326ffe82f4e0f1d
ductran <ductran1905@gmail.com>
2018-06-17 07:56:53 +0700

    Stringer

---
+++ packages/excercise-stringer.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+"fmt"
+"strconv"
+)
+
+type IPAddr [4]byte
+
+func (ip IPAddr) String() string {
+	return strconv.Itoa(int(ip[0])) + "." + strconv.Itoa(int(ip[1])) + "." + strconv.Itoa(int(ip[2])) + "." + strconv.Itoa(int(ip[3]))
+}
+
+func main() {
+	hosts := map[string]IPAddr {
+		"lookback": {127,0,0,1},
+		"google": {8,8,8,8},
+	}
+
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
