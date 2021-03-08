4a644ec60cc603f357304b12d83edf3492c9b792
davidtran <davidtran641@gmail.com>
2021-03-08 23:26:47 +0800

    Add time out

--- gowithtest/concurency/check_websites.go
+++ gowithtest/concurency/check_websites.go
@@ -1,5 +1,9 @@
 package concurrency
 
+import (
+	"time"
+)
+
 type WebsiteChecker func(string) bool
 
 type result struct {
@@ -17,9 +21,15 @@ func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 		}(url)
 	}
 
+	timeout := time.After(100 * time.Microsecond)
+
 	for i := 0; i < len(urls); i++ {
-		result := <-resultChannel
-		results[result.url] = result.res
+		select {
+		case result := <-resultChannel:
+			results[result.url] = result.res
+		case <-timeout:
+			return results
+		}
 	}
 
 	return results
