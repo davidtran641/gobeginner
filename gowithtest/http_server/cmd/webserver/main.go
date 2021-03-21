258143685f5ae8bb220d712ba911868bc3d33dc8
davidtran <davidtran641@gmail.com>
2021-03-21 09:45:46 +0800

    Update structure

---
+++ gowithtest/http_server/cmd/webserver/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/davidtran641/gobeginner/gowithtest/http_server/server"
+)
+
+const (
+	dbFileName = "game.db.json"
+)
+
+func main() {
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalf("Can't create database %s: %v", dbFileName, err)
+	}
+
+	store, err := server.NewFileSystemPlayerStore(db)
+	if err != nil {
+		log.Fatal("Can't create store")
+	}
+
+	s := server.NewPlayerServer(store)
+	handler := http.HandlerFunc(s.ServeHTTP)
+
+	if err := http.ListenAndServe(":5000", handler); err != nil {
+		log.Fatal("Could not listen on port 5000. ", err)
+	}
+}
